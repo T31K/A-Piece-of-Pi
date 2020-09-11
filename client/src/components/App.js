46f3ebbe71bbb3a5cb5b746f161ff0f2d4fd9201
Teik Mun Wong <t31k@Teiks-MacBook-Air-2.local>
2020-09-12 00:17:07 +0800

    Added routes on client

---
+++ client/src/components/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+import Home from "./Home"
+import About from "./About"
+import Blog from "./Blog"
+import '../App.css';
+
+class App extends Component{
+  render(){
+      return (
+        <Router>
+            <div className ="bg">
+              <Navbar className="bg " variant="dark" align="center" >
+              <Navbar.Brand href="/">APOP</Navbar.Brand>
+                <Nav className="mr-auto">
+                  <li><Link to={'/'} className="nav-link"> /home </Link></li>
+                  <li><Link to={'/blog'} className="nav-link"> /blog </Link></li>
+                  <li><Link to={'/about'} className="nav-link"> /about </Link></li>
+                </Nav>
+              </Navbar>
+              
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/blog' component={Blog} />
+                <Route exact path='/about' component={About} />
+              </Switch>  
+            </div>
+        </Router>
+
+      )
+    }
+}
+
+export default App;
---
+++ client/src/components/Blog.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react'
+import Container from "react-bootstrap/Container"
+
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import "../App.css"
+class Blog extends Component {
+    constructor(props){
+      super(props)
+  
+      this.state = {
+        posts: [],
+      }
+    }
+  
+    componentDidMount(){
+      fetch("http://localhost:3001/")
+      .then(res => res.json())
+      .then(res => this.setState({posts: res}))
+    }
+  
+  
+    render(){
+        return(
+        <Container className="pl-5 pr-5 mx-auto">
+            <Row>
+            <Col>
+                {this.state.posts.map( (post,index) => (
+                
+
+                <div className="window-inner mb-4 mx-auto" key={index}>
+                    <div className="icons"><span></span></div>
+                    <div className="title">>_ {post.title.toLowerCase()}</div>
+                        <pre className="lang-python">   
+                            <code>
+                                <img className="picture rounded mx-auto" src={post.image} alt="image"/>
+                            </code>
+                        </pre>
+                      
+                </div>
+                    ))}
+    
+          
+            
+              
+            </Col>
+            </Row>
+            </Container>
+        )
+    }
+}
+        
+export default Blog 
