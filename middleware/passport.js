8a0f30de13e5e71c24bd5553780bac5112186116
T31K <t31kmunwong@gmail.com>
2020-09-28 13:33:21 +0800

    first

--- middleware/passport.js
+++ middleware/passport.js
@@ -3,6 +3,7 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models/User");
 const SECRET = process.env.SECRET;
 
+
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = SECRET;
